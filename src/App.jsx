4b9599844db9b207e519042ccec352d68ede3011
unknown <filip.trifunovic23@gmail.com>
2023-12-07 13:44:44 +0100

    setting basic files, and routes

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
