adb35059a87af48303b42729f0edab7bd08d1e67
unknown <filip.trifunovic23@gmail.com>
2023-12-08 13:22:31 +0100

    fetching order with id

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from "./UI/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./UI/AppLayout";
+import Error from "./UI/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -26,7 +29,9 @@ const router = createBrowserRouter([
       },
       {
         path: "/order/:orderId",
+        loader: orderLoader,
         element: <Order />,
+        errorElement: <Error />,
       },
       {
         path: "/order/new",
---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="search order number"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
